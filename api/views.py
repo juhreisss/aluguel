3f7dc8e975637d559ca92e051fe21f5a62ce3139
juhreis2508@gmail.com <julia.reis32@senaisp.edu.br>
2026-02-02 11:32:29 -0300

    Terceira versão

--- api/views.py
+++ api/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Usuario, Imovel, Contrato, Pagamento
+from .serializers import (UsuarioSerializer, ImovelSerializer,  ContratoSerializer, PagamentoSerializer
+)
 
-# Create your views here.
+# class UsuarioListCreateAPIView(APIView):
+#     def get(self, request):
+#         usuarios = Usuario.objects.all()
+#         serializer = UsuarioSerializer(usuarios, many=True)
+#         return Response(serializer.data)
+
+class UsuarioListCreateAPIView(ListCreateAPIView):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
